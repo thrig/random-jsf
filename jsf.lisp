ad5052eb396789d27d35a7917c70172248c330f6
Jeremy Mates <jeremy.mates@gmail.com>
2020-04-26 14:00:10 -0700

    some small improvements
     * local variables in jrandom
     * use :initial-element to set seed instead of psetf

--- jsf.lisp
+++ jsf.lisp
@@ -8,30 +8,28 @@
 ; TODO this probably needs LDB32 applied to it for 32-bit LISPs
 (defmacro rot32 (x k) `(logior (ash ,x ,k) (ash ,x (- (- 32 ,k)))))
 
-; TODO b, c, d will need local storage if multi-threaded? but don't know
-; how to make the array changes atomic in such a case...
 (defun jrandom (ctx)
   (declare (optimize (speed 3) (safety 1) (debug 1))
            ((simple-array (unsigned-byte 32) (5)) ctx))
-  (let ((e (ldb32 (- (aref ctx 1) (rot32 (aref ctx 2) 27)))))
+  (let* ((b (aref ctx 2))
+         (c (aref ctx 3))
+         (d (aref ctx 4))
+         (e (ldb32 (- (aref ctx 1) (rot32 b 27)))))
     (declare ((unsigned-byte 32) e))
-    (setf (aref ctx 1) (logxor (aref ctx 2) (ldb32 (rot32 (aref ctx 3) 17))))
-    (setf (aref ctx 2) (ldb32 (+ (aref ctx 3) (aref ctx 4))))
-    (setf (aref ctx 3) (ldb32 (+ (aref ctx 4) e)))
-    (setf (aref ctx 4) (ldb32 (+ e (aref ctx 1))))))
+    (let ((a (setf (aref ctx 1) (logxor b (ldb32 (rot32 c 17))))))
+      (setf (aref ctx 2) (ldb32 (+ c d)))
+      (setf (aref ctx 3) (ldb32 (+ d e)))
+      (setf (aref ctx 4) (ldb32 (+ e a))))))
 
 (defun jrandom-seed (ctx)
   (declare ((simple-array (unsigned-byte 32) (5)) ctx))
   (aref ctx 0))
 
+; slot 0 is original seed; A => 1, B => 2, C => 3, D => 4
 (defun make-random-jsf (seed)
   (declare ((unsigned-byte 32) seed))
-  (let ((ctx (make-array 5 :element-type '(unsigned-byte 32))))
-    (setf seed (ldb32 seed))
-    (psetf (aref ctx 0) seed       ; original seed
-           (aref ctx 1) #xf1ea5eed ; A
-           (aref ctx 2) seed       ; B
-           (aref ctx 3) seed       ; C
-           (aref ctx 4) seed)      ; D
+  (let ((ctx (make-array 5 :element-type '(unsigned-byte 32)
+                           :initial-element (ldb32 seed))))
+    (setf (aref ctx 1) #xf1ea5eed)
     (loop :repeat 20 :do (jrandom ctx))
     ctx))
